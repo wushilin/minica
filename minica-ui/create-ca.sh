98cb6764a7def3c43424cd6dfc89749f2118fdc6
Shilin Wu <shwu@confluent.io>
2023-12-08 13:25:33 +0800

    Fixes the shell script with CSRF attack

--- minica-ui/create-ca.sh
+++ minica-ui/create-ca.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-curl -H "Content-Type: application/json"  -X PUT --data @create-ca.json http://localhost:4200/ca/new
+curl -H "X-XSRF-TOKEN: 123" -H "Cookie: XSRF-TOKEN=123;" -H "Content-Type: application/json"  -X PUT --data @create-ca.json http://localhost:4200/ca/new
